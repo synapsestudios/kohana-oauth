48d3060d4dac54780def9dad74e427482debfedb
Woody Gilk <woody.gilk@gmail.com>
2011-04-13 11:26:11 -0500

    Added OAuth2 example to config/oauth

--- config/oauth.php
+++ config/oauth.php
@@ -8,8 +8,17 @@ return array(
 	 * You will be given a "consumer key" and "consumer secret", which must
 	 * be provided when making OAuth requests.
 	 */
-	'twitter' => array(
-		'key' => 'your consumer key',
-		'secret' => 'your consumer secret'
-	),
+	// 'twitter' => array(
+	// 	'key' => 'your consumer key',
+	// 	'secret' => 'your consumer secret'
+	// ),
+	/**
+	 * Github applications can be registered at https://github.com/account/applications/new.
+	 * You will be given a "client id" and "client secret", which must
+	 * be provided when making OAuth2 requests.
+	 */
+	// 'github' => array(
+	// 	'id' => 'your client id',
+	// 	'secret' => 'your client secret'
+	// ),
 );
