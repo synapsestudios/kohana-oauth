27b17f2b169d7cfde7323da8f3b5069cb69cd0ae
Woody Gilk <woody.gilk@kohanaframework.org>
2011-12-16 18:18:09 -0600

    Added MailChimp provider

---
+++ classes/kohana/oauth2/provider/mailchimp.php
@@ -0,0 +1,39 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+
+abstract class Kohana_OAuth2_Provider_Mailchimp extends OAuth2_Provider {
+
+	public $name = 'mailchimp';
+
+	public function url_authorize()
+	{
+		return 'https://login.mailchimp.com/oauth2/authorize';
+	}
+
+	public function url_access_token()
+	{
+		return 'https://login.mailchimp.com/oauth2/token';
+	}
+
+	public function url_metadata()
+	{
+		return 'https://login.mailchimp.com/oauth2/metadata';
+	}
+
+	public function metadata(OAuth2_Token_Access $token, array $params = NULL)
+	{
+		$request = OAuth2_Request::factory('resource', 'GET', $this->url_metadata(), array(
+				'access_token' => $token->token,
+			))
+			->required('access_token', TRUE)
+			;
+
+		if ($params)
+		{
+			// Load user parameters
+			$request->params($params);
+		}
+
+		return $this->execute($request);
+	}
+
+}
